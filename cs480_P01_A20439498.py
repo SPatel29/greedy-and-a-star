383c6d7675047322806b88dfb2298f01bc5b7828
SPatel29 <sunnypatelc9@gmail.com>
2022-02-27 17:50:53 -0600

    created action method and a get_cost method

--- cs480_P01_A20439498.py
+++ cs480_P01_A20439498.py
@@ -9,48 +9,44 @@ class Problem:  # think we need adjacency matrix in this class?
     def __init__(self, initial, goal):
         self.initial = initial
         self.goal = goal
-        self.state_space = {} #all possible states environment can be in. Contains driving file data
+        # all possible states environment can be in. Contains driving file data
+        self.state_space = {}
 
-    def read_file(self, file):
+    def generate_state_space(self, file):
         col_lst = []
         row_lst = []
-        my_dict = {}
+        data_lst = []
 
         with open(file, 'r') as f:
             data = list(csv.reader(f, delimiter=','))
         data = py.array(data)
-        data_lst = []
-        data_lst = data.tolist()    #a list within a list. Each sublist is a line from file
+        data_lst = data.tolist()  # a list within a list. Each sublist is a line from file
 
         col_lst = data_lst[0][1:]
         for element in data_lst[1:]:
             row_lst.append(element[0])
 
-        for element in data_lst[1:]:    #element is a list
-            self.state_space[element[0]] = {}   #create nested dictionary
-            for i in range(len(element)):   
+        for element in data_lst[1:]:  # element is a list
+            self.state_space[element[0]] = {}  # create nested dictionary
+            for i in range(len(element)):
+                # if not row header (i.e is a driving distance)
                 if element[i] not in row_lst:
-                    self.state_space[element[0]][col_lst[i-1]] = element[i]   #elemtn[0] is the row header for each line. col)lst[i] is the col header from col_list
+                    self.state_space[element[0]
+                                     ][col_lst[i-1]] = int(element[i])
+                    # element[0] is the row header for each line. col)lst[i] is the col header from col_list
+                    # subtract by one because we do not want we are skipping over first index of element since it
+                    # does not contain a data value. the first index contains the row header, not numeber distance
+        print(self.state_space["WA"]["ID"])
+    def get_cost(self, state_from, state_to):
+        return self.state_space[state_from][state_to]
 
-        
-        #TODO:
-        #   get data from file
-        #   
-    def state_space(self, file):  # make the adjacency matrix. set of possible states environment can be in
-        # x axis of state space (2D array) will be currenty looking state
-        # y axis of state space (2D array) will be destination/next state
-        pass
-
-    def transitional_model(self):  # already 'built in' into the adjacency matrix
-        pass
-
-    def action_cost_function(self, state_a, state_b):  # function that reads you cost of going from state_a to state_b
-        # action cost function is a mechanism  of reading the cost (weight) corresponding to two
-        # states x and y (think row and column) from the corresponding cell in the adjcanecy matrix
-        # i.e,
-        # state[x][y] = cost
-        # recall state space tree is a 2D array
-        pass
+    # returns a list of all possible states it can traverse.
+    def actions(self, state):
+        temp_lst = []
+        for key, value in self.state_space[state].items():
+            if value > 0:
+                temp_lst.append(key)
+        return temp_lst
 
     def get_initial_state(self):
         return self.initial
@@ -66,11 +62,11 @@ class Node:
 
 
 def main():
-    if len(sys.argv) == 3:      
+    if len(sys.argv) == 3:
         initial_state = (sys.argv[1])
         goal_state = (sys.argv[2])
         problem = Problem(initial_state, goal_state)
-        problem.read_file("greedy-and-a-star/driving(1).csv")
+        problem.generate_state_space("greedy-and-a-star/driving(1).csv")
         # problem = Problem(initial_state, goal_state)
     else:
         raise Exception('ERROR: Not enough or too many arguments')
